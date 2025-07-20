0fae183e293ad777d4902d097d0af4bc73d802cd
sabinanfranz <geonho.shin1@gmail.com>
2025-07-20 14:53:05 +0900

    모든 새로 작성한 코드 업로드 - 대시보드 완전 개선

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import streamlit as st            # 1) 반드시 가장 먼저!
 st.set_page_config(page_title="Deal Dashboard", layout="wide")
 
 st.markdown("## DAY1 B2B 대시보드")
-st.markdown("###### - 좌측 사이드바에서 원하는 대시보드를 선택해주세요.")
+st.markdown("###### - 좌측 사이드바에서 원하시는 대시보드를 선택해주세요.")
