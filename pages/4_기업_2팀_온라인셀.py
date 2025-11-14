20c558d1aeb923692328da2f56fc06d52b9c077d
sabinanfranz <geonho.shin1@gmail.com>
2025-11-14 14:49:35 +0900

    Add ?⑤씪?몄? organization and update deal files

---
+++ pages/4_기업_2팀_온라인셀.py
@@ -0,0 +1,7 @@
+# pages/4_기업_2팀_온라인셀.py
+import streamlit as st
+from _part_view_base import render_part
+
+st.set_page_config(page_title="기업 2팀 온라인셀", layout="wide")
+render_part("기업교육 2팀", "온라인셀")
+
